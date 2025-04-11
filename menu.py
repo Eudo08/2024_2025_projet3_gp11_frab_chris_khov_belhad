7c06a96737d505c0cba852f6bd7d927465eae635
MAMARINEEE <marine.fraboulet@hotmail.com>
2025-04-11 13:10:24 +0200

    avance menu

--- menu.py
+++ menu.py
@@ -2,6 +2,11 @@ import pygame
 
 pygame.init()
 
+NOIR = (0, 0, 0)
+BLANC = (255, 255, 255)
+GRIS = (193, 193, 193)
+BLEU = (0, 150, 255)
+
 ecran = pygame.display.set_mode((800, 600))
 
 continuer = True
@@ -10,5 +15,8 @@ while continuer:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             continuer = False
+
+ecran.fill(NOIR)
+clock = pygame.time.Clock()
 
 pygame.quit()
