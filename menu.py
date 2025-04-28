b391478f71d6186b89c044063ab8793261d5beaf
MAMARINEEE <marine.fraboulet@hotmail.com>
2025-04-28 10:53:45 +0200

    new color

--- menu.py
+++ menu.py
@@ -6,17 +6,21 @@ NOIR = (0, 0, 0)
 BLANC = (255, 255, 255)
 GRIS = (193, 193, 193)
 BLEU = (0, 150, 255)
+BLEU_FONCE = (2, 25, 98)
 
 ecran = pygame.display.set_mode((800, 600))
 
-continuer = True
-
-while continuer:
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            continuer = False
-
-ecran.fill(NOIR)
+running = True
 clock = pygame.time.Clock()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    ecran.fill(BLEU_FONCE)
+    pygame.display.update()
+
+    clock.tick(60)
 
 pygame.quit()
