52c167cc27e2a6f508650554cce14e903cf0ac0c
MAMARINEEE <marine.fraboulet@hotmail.com>
2025-04-28 11:42:32 +0200

    new title

--- menu.py
+++ menu.py
@@ -9,6 +9,9 @@ BLEU = (0, 150, 255)
 BLEU_FONCE = (2, 25, 98)
 
 ecran = pygame.display.set_mode((800, 600))
+font = pygame.font.Font(None, 74)
+
+player_pos = pygame.Vector2(ecran.get_width() / 2, ecran.get_height() / 4)
 
 running = True
 clock = pygame.time.Clock()
@@ -19,8 +22,15 @@ while running:
             running = False
     
     ecran.fill(BLEU_FONCE)
-    pygame.display.update()
 
+    texte = font.render("TETRIS", True, NOIR)
+    texte_rect = texte.get_rect(center=player_pos)
+    ecran.blit(texte, texte_rect)
+
+    pygame.display.flip()
+    
     clock.tick(60)
 
 pygame.quit()
+
+
