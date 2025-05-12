65219dba88a7f00ac5173e5d6f665b60839ef946
MAMARINEEE <marine.fraboulet@hotmail.com>
2025-05-12 12:13:01 +0200

    avance

--- menu.py
+++ menu.py
@@ -29,43 +29,73 @@ def draw_button(ecran, text, x, y, w, h, color, text_color):
     ecran.blit(text_surface, text_rect)
 
 
+# # while running:
+# #     ecran.fill(NOIR)
+# #     draw_button(ecran, (300, 250, 200, 50), "Jouer !", BLANC, ROUGE)
+# #     texte = font.render("TETRIS", True, BLANC)
+# #     texte_rect = texte.get_rect(center=player_pos)
+# #     ecran.blit(texte, texte_rect)
+
+# #     for event in pygame.event.get():
+# #         if event.type == pygame.QUIT:
+# #             running = False
+# #             sys.exit()
+# #         elif event.type == pygame.MOUSEBUTTONDOWN:
+# #                 if 300 <= event.pos[0] <= 500 and 250 <= event.pos[1] <= 300:
+# #                     en_cours()
+
+# etat_jeu = "menu"
+
 # while running:
 #     ecran.fill(NOIR)
-#     draw_button(ecran, (300, 250, 200, 50), "Jouer !", BLANC, ROUGE)
-#     texte = font.render("TETRIS", True, BLANC)
-#     texte_rect = texte.get_rect(center=player_pos)
-#     ecran.blit(texte, texte_rect)
 
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             running = False
-#             sys.exit()
-#         elif event.type == pygame.MOUSEBUTTONDOWN:
+#     if etat_jeu == "menu":
+#         draw_button(ecran, "Jouer !", 300, 250, 200, 50, BLANC, ROUGE)
+#         texte = font.render("TETRIS", True, BLANC)
+#         texte_rect = texte.get_rect(center=player_pos)
+#         ecran.blit(texte, texte_rect)
+
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 running = False
+#                 sys.exit()
+#             elif event.type == pygame.MOUSEBUTTONDOWN:
 #                 if 300 <= event.pos[0] <= 500 and 250 <= event.pos[1] <= 300:
-#                     en_cours()
+#                     etat_jeu = "jeu"  # Change l'état du jeu
 
-etat_jeu = "menu"
+#     elif etat_jeu == "jeu":
+#         en_cours()
 
-while running:
-    ecran.fill(NOIR)
+# pygame.quit()
 
-    if etat_jeu == "menu":
-        draw_button(ecran, "Jouer !", 300, 250, 200, 50, BLANC, ROUGE)
-        texte = font.render("TETRIS", True, BLANC)
-        texte_rect = texte.get_rect(center=player_pos)
-        ecran.blit(texte, texte_rect)
+def show_menu():
+    buton_color = (ROUGE)
+    hover_color = (BLEU)
+    button_width, button_height = 200, 50
+    
+    pass
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                sys.exit()
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                if 300 <= event.pos[0] <= 500 and 250 <= event.pos[1] <= 300:
-                    etat_jeu = "jeu"  # Change l'état du jeu
+while running: 
+    ecran.fill(NOIR) 
+    texte = font.render("TETRIS", True, BLANC) 
+    texte_rect = texte.get_rect(center=player_pos)
+    ecran.blit(texte, texte_rect)
+    draw_button(BLANC, (300, 250, 200, 50), "Jouer !", BLANC) 
 
-    elif etat_jeu == "jeu":
-        en_cours()
-
-pygame.quit()
-
-
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: 
+            running = False 
+            sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN: 
+            if 300 <= event.pos[0] <= 500 and 250 <= event.pos[1] <= 300: 
+                en_cours() 
+    
+    ecran.fill(NOIR) 
+    texte = font.render("TETRIS", True, BLANC) 
+    texte_rect = texte.get_rect(center=player_pos)
+    ecran.blit(texte, texte_rect) 
+                                                            
+    pygame.display.flip() 
+    clock.tick(60) 
+    
+    pygame.quit()
