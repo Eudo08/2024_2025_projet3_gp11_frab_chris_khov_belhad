36a48df4b2c602eb3b26155cf39b84e70b2613a5
MAMARINEEE <marine.fraboulet@hotmail.com>
2025-05-20 09:07:03 +0200

    new button

--- menu.py
+++ menu.py
@@ -1,6 +1,7 @@
 import pygame
 import sys 
 import tetris_ia
+import tetris_j
 from tools import Button
 
 pygame.init()
@@ -23,12 +24,18 @@ player_pos = pygame.Vector2(ecran.get_width() / 2, ecran.get_height() / 4)
 clock = pygame.time.Clock()
 
 def show_menu(): 
+
     button_color = (ROUGE)
     hover_color = (BLEU)
     button_width, button_height = 200, 50
+
     start_x = ecran.get_width() / 2 - ecran.get_height() / 2
-    player_button = Button("Joueur", start_x, 200, button_width, button_height, button_color, hover_color)
+
+    ia_button = Button("IA", start_x, 200, button_width, button_height, button_color, hover_color)
+    player_button = Button ("Jouer !", start_x, 200, button_width, button_height, button_color, hover_color)
+
     running = True
+
     while running:
         ecran.fill(NOIR)
 
@@ -47,11 +54,13 @@ def show_menu():
                 if event.button == 1:
                     mouse_clicked = True
 
+        ia_button.update(mouse_pos)
         player_button.update(mouse_pos)
 
+        ia_button.draw(ecran)
         player_button.draw(ecran)
         
-        if player_button.check_clicked (mouse_pos, mouse_clicked):
+        if ia_button.check_clicked (mouse_pos, mouse_clicked):
             running = False
             tetris_ia()
 
